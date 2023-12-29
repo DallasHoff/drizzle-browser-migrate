31b32ce25ee9571d1f85947c83a006e1c275866f
Dallas Hoffman <dallashoff@gmail.com>
2023-12-29 13:07:01 -0500

    Add drizzle-kit config; update dependencies

--- src/db.ts
+++ src/db.ts
@@ -2,6 +2,5 @@ import { SQLocalDrizzle } from 'sqlocal/drizzle';
 import { drizzle } from 'drizzle-orm/sqlite-proxy';
 
 // Initialize Drizzle with SQLocal driver
-const { driver } = new SQLocalDrizzle('database.sqlite3');
+const { driver } = new SQLocalDrizzle('migrations-test.sqlite3');
 export const db = drizzle(driver);
-
